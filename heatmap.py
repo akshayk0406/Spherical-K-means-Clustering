506bb3a5f94c2e3ff2d5dc52150fec5e7e8150aa
Akshay Kulkarni <akshaykulkarni@Akshays-MacBook-Pro.local>
2015-11-07 19:03:03 -0600

    Script to generate HeatMap for clustering solution

---
+++ heatmap.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+entropy_file = sys.argv[1]
+i = 1
+x = []
+y = []
+intensity = []
+with open(entropy_file,'r') as f:
+	for line in f:
+		intensity.append([int(freq.strip()) for freq in line.split(" ")[:-1]])
+		x.append(i)
+		y.append(i*0.1)
+		i = i+1
+
+#setup the 2D grid with Numpy
+x, y = np.meshgrid(x, y)
+
+#convert intensity (list of lists) to a numpy array for plotting
+intensity = np.array(intensity)
+
+#now just plug the data into pcolormesh, it's that easy!
+plt.pcolormesh(x, y, intensity)
+plt.colorbar() #need a colorbar to show the intensity scale
+plt.savefig('clustering_heatmap.png')
